fd38a74ebe776374033fee444c4866c4cd88953b
onuki <k022g0020@g.neec.ac.jp>
2023-04-14 12:52:05 +0900

    00_01完了

--- Header/Math/Vector3.cpp
+++ Header/Math/Vector3.cpp
@@ -1,1 +1,9 @@
-//#include "Vector3.hpp"
+#include "Vector3.hpp"
+#include "Matrix2x2.hpp"
+#include <Novice.h>
+
+void Vector3::Printf(int x_, int y_) const {
+	Novice::ScreenPrintf(x_, y_, "%12.2f // %6.2f", this->x, Length());
+	Novice::ScreenPrintf(x_, y_ + TextHeight, "%12.2f", this->y);
+	Novice::ScreenPrintf(x_, y_ + TextHeight * 2, "%12.2f", this->z);
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include <Novice.h>
 #include <Header/Math/Vector2.hpp>
 #include <Header/Math/Vector3.hpp>
+#include <Novice.h>
+
+#include <imgui.h>
 
 const char kWindowTitle[] = "LE2A_03_オヌキ_セイヤ_MT3";
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(_In_ HINSTANCE,_In_opt_ HINSTANCE,_In_ LPSTR,_In_ int) {
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
@@ -13,6 +15,11 @@ int WINAPI WinMain(_In_ HINSTANCE,_In_opt_ HINSTANCE,_In_ LPSTR,_In_ int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	// 変数
+	Vector3 v1{1.f, 3.f, -5.f};
+	Vector3 v2{4.f, -1.f, 2.f};
+	float k{4.f};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -27,6 +34,29 @@ int WINAPI WinMain(_In_ HINSTANCE,_In_opt_ HINSTANCE,_In_ LPSTR,_In_ int) {
 		/// ↓更新処理ここから
 		///
 
+		ImGui::Begin("config");
+		ImGui::Text("vector1");
+		ImGui::InputFloat("v1.x", &v1.x);
+		ImGui::InputFloat("v1.y", &v1.y);
+		ImGui::InputFloat("v1.z", &v1.z);
+		ImGui::Text("vector2");
+		ImGui::InputFloat("v2.x", &v2.x);
+		ImGui::InputFloat("v2.y", &v2.y);
+		ImGui::InputFloat("v2.z", &v2.z);
+		ImGui::Text("k");
+		ImGui::InputFloat("k", &k);
+		ImGui::End();
+
+		Vector3 add = v1 + v2;
+		Vector3 subtract = v1 - v2;
+
+		Vector3 multiply = v1 * k;
+
+		float dot = v1 * v2;
+		float length = v1.Length();
+
+		Vector3 nomalize = v2.Nomalize();
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -34,6 +64,13 @@ int WINAPI WinMain(_In_ HINSTANCE,_In_opt_ HINSTANCE,_In_ LPSTR,_In_ int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		add.Printf(0, 0);
+		subtract.Printf(0, 70);
+		multiply.Printf(0, 140);
+		Novice::ScreenPrintf(0, 220, "%.3f", dot);
+		Novice::ScreenPrintf(0, 240, "%.3f", length);
+		nomalize.Printf(0, 260);
 
 		///
 		/// ↑描画処理ここまで
