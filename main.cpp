a2b9bf7c2c0671c2708f0bf067a035154263bd58
onuki <k022g0020@g.neec.ac.jp>
2023-04-21 12:43:00 +0900

    mainの掃除

--- main.cpp
+++ main.cpp
@@ -18,24 +18,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	Matrix4x4 m1 = {3.2f, 0.7f, 9.6f, 4.4f, 5.5f, 1.3f, 7.8f, 2.1f,
-	                6.9f, 8.f,  2.6f, 1.f,  0.5f, 7.2f, 5.1f, 3.3f};
-	Matrix4x4 m2 = {4.1f, 6.5f, 3.3f, 2.2f, 8.8f, 0.6f, 9.9f, 7.7f,
-	                1.1f, 5.5f, 6.6f, 0.0f, 3.3f, 9.9f, 8.8f, 2.2f};
-
-	Matrix4x4 add = m1 + m2;
-	Matrix4x4 subtract = m1 - m2;
-	Matrix4x4 multiply = m1 * m2;
-
-	Matrix4x4 inverseM1 = m1.Inverse();
-	Matrix4x4 inverseM2 = m2.Inverse();
-
-	Matrix4x4 transposeM1 = m1.Transpose();
-	Matrix4x4 transposeM2 = m2.Transpose();
-
-	Matrix4x4 identity = Matrix4x4::Identity();
-
-	bool toggle = false;
+	// 変数
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -50,13 +33,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		/// ↓更新処理ここから
 		///
 
-		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
-			if (toggle) {
-				toggle = false;
-			} else {
-				toggle = true;
-			}
-		}
+		
 
 		///
 		/// ↑更新処理ここまで
@@ -65,18 +42,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓描画処理ここから
 		///
-		if (toggle) {
-			add.Printf(0, 0);
-			subtract.Printf(0, 100);
-			multiply.Printf(0, 200);
-			inverseM1.Printf(0, 300);
-			inverseM2.Printf(0, 400);
-		} else {
-			transposeM1.Printf(600, 0);
-			transposeM2.Printf(600, 100);
-			identity.Printf(600, 200);
-		}
-		Novice::ScreenPrintf(600, 400, "Toggle Space");
+		
+
 
 		///
 		/// ↑描画処理ここまで
